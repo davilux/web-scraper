50fa970ad78ef0f8ec2216249ac9d9a11ac9d9f9
davilux <mx.davilux@gmail.com>
2023-03-21 15:27:57 -0400

    Separated functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+## Web Scraper
+
+A simple example of Puppeteer, this program allows us to scrape sites and store data in .json files.
--- index.js
+++ index.js
@@ -5,66 +5,91 @@ const fs = require('fs')
 
 //All puppeteer operations are asynchronous, so wrap them in an async function:
 const scrape = async () => {
+
+  //Accept url as an argument:
+  let url = process.argv[2]
+  if(!url) url = 'http://books.toscrape.com/'
+
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('http://books.toscrape.com/')
+
+  await page.goto(url)
 
 
-  ////Take a screenshot:
-  // await page.screenshot({path:'bookSandbox.png', fullPage:true})
+  // Take a screenshot:
+  const takeScreenshot = async () => {
+    await page.screenshot({path:'bookSandbox.png', fullPage:true})
+  }
 
-  ////Create a pdf:
-  // await page.pdf({path:'bookSandbox.pdf', format:'A4'})
+  // Make a pdf:
+  const makePdf = async () => {
+    await page.pdf({path:'bookSandbox.pdf', format:'A4'})
+  }
 
-  ////Get the title of the page:
-  // const html = await page.content()
-  // const title = await page.evaluate(() => document.title)
-  // console.log(title)
+  // Log the title of the page:
+  const logPageTitle = async () => {
+    const html = await page.content()
+    const title = await page.evaluate(() => document.title)
+    console.log(title)
+  }
 
-  ////Get all the text:
-  // const html = await page.content()
-  // const text = await page.evaluate(() => document.body.innerText)
-  // console.log(text)
+  // Log all page text:
+  const logAllPageText = async () => {
+    const html = await page.content()
+    const text = await page.evaluate(() => document.body.innerText)
+    console.log(text)
+  }
 
-  // //Get all the links:
-  // const links = await page.evaluate(() => {
-  //   return Array.from(document.querySelectorAll('a'), (element) => element.href )
-  // })
-  // console.log(links)
+  // Log all the links:
+  const logAllLinks = async () => {
+    const links = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll('a'), (element) => element.href )
+    })
+    console.log(links)
+  }
 
-  //   //Get all the book titles.
-  //   //Array.from() creates a new shallow-copied array from an array-like object. The callback specifies what information to include in the new array.
-  // const titles = await page.evaluate(() => {
-  //   return Array.from(document.querySelectorAll('.product_pod h3 a'), (element) => element.title)
-  // })
-  // console.log(titles)
+  // Log all book titles:
+  // Array.from() creates a new shallow-copied array from an array-like object. The callback specifies what information to include in the new array.
 
+  const logAllTitles = async () => {
+    const titles = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll('.product_pod h3 a'), (element) => element.title)
+    })
+    console.log(titles)
+  }
 
-  // //Get all the book data:
-  // const books = await page.evaluate(() => {
-  //   return Array.from(document.querySelectorAll('.product_pod'), (element) => ({
-  //     title: element.querySelector('h3 a').title,
-  //     link: element.querySelector('h3 a').href,
-  //     price: element.querySelector('.product_price .price_color').innerText,
-  //     starRating: element.querySelector('.star-rating').classList['1']
-  //   }))
-  // })
-  // console.log(books)
-
-  //Different way to accomplish the same thing. We can use $$eval and .map instead of Array.from()
-  const books = await page.$$eval('.product_pod', (elements) => elements.map(element => ({
-    title: element.querySelector('h3 a').title,
-    link: element.querySelector('h3 a').href,
-    price: element.querySelector('.product_price .price_color').innerText,
-    starRating: element.querySelector('.star-rating').classList['1']
-  })))
-
-  //Save data to a .JSON file:
-  fs.writeFile('books.json', JSON.stringify(books), (err)=> {
-    if(err) throw err
-    console.log('file saved')
+  // Log all book data:
+  const logBookData = async () => {
+  const books = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('.product_pod'), (element) => ({
+      title: element.querySelector('h3 a').title,
+      link: element.querySelector('h3 a').href,
+      price: element.querySelector('.product_price .price_color').innerText,
+      starRating: element.querySelector('.star-rating').classList['1']
+    }))
   })
+  console.log(books)
+  }
+  // Save book data to a file:
+  const saveBookDataToFile = async (filename) => {
 
+    //We can use $$eval and .map instead of Array.from()
+    const books = await page.$$eval('.product_pod', (elements) => elements.map(element => ({
+      title: element.querySelector('h3 a').title,
+      link: element.querySelector('h3 a').href,
+      price: element.querySelector('.product_price .price_color').innerText,
+      starRating: element.querySelector('.star-rating').classList['1']
+    })))
+
+
+      //Save data to a .JSON file:
+      fs.writeFile(filename, JSON.stringify(books), (err)=> {
+        if(err) throw err
+        console.log('file saved')
+      })
+  }
+
+  await saveBookDataToFile('books.json')
 
   //always close the browser when you're done.
   await browser.close()
