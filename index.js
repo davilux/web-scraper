0515aa1103943c73f880d4347d080e6e59fbcd01
davilux <mx.davilux@gmail.com>
2023-03-21 11:27:16 -0400

    Basic scraper implemented

---
+++ index.js
@@ -0,0 +1,59 @@
+const puppeteer = require('puppeteer')
+
+//Use the fs (file system) package to write what we scrape to files.
+const fs = require('fs')
+
+//All puppeteer operations are asynchronous, so wrap them in an async function:
+const scrape = async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('http://books.toscrape.com/')
+
+
+  ////Take a screenshot:
+  // await page.screenshot({path:'screenshot.png', fullPage:true})
+
+  ////Create a pdf:
+  // await page.pdf({path:'example.pdf', format:'A4'})
+
+  ////Get the title of the page:
+  // const html = await page.content()
+  // const title = await page.evaluate(() => document.title)
+  // console.log(title)
+
+  ////Get all the text:
+  // const html = await page.content()
+  // const text = await page.evaluate(() => document.body.innerText)
+  // console.log(text)
+
+  // //Get all the links:
+  // const links = await page.evaluate(() => {
+  //   return Array.from(document.querySelectorAll('a'), (element) => element.href )
+  // })
+  // console.log(links)
+
+  //   //Get all the book titles.
+  //   //Array.from() creates a new shallow-copied array from an array-like object. The callback specifies what information to include in the new array.
+  // const titles = await page.evaluate(() => {
+  //   return Array.from(document.querySelectorAll('.product_pod h3 a'), (element) => element.title)
+  // })
+  // console.log(titles)
+
+
+      //Get all the book data:
+      const books = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('.product_pod'), (element) => ({
+          title: element.querySelector('h3 a').title,
+          link: element.querySelector('h3 a').href,
+          price: element.querySelector('.product_price .price_color').innerText,
+          starRating: element.querySelector('.star-rating').classList['1']
+        }))
+      })
+      console.log(books)
+
+
+  //always close the browser when you're done.
+  await browser.close()
+}
+
+scrape()
